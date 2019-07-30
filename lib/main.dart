4695ec60aa06a674f0f48e783f3a3995db21e207
AMOREM <amorem@amorem.com>
2019-07-30 18:47:22 +0200

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+import 'question.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What is you favorite color ?',
+      "What is your favorite animal"
+    ];
     return MaterialApp(
-      home: Text('Hello World'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first App"),
+        ),
+        body: Column(
+          children: [
+            Question(questions[_questionIndex]),
+            RaisedButton(
+              child: Text("Answer1"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer2"),
+              onPressed: _answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer3"),
+              onPressed: _answerQuestion,
+            )
+          ],
+        ),
+      ),
     );
   }
 }
